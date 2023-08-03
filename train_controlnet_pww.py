ac0df926ae45070f36b4ceff9308b06683b95534
Denis lukovnikov <lukovnikov.denis@gmail.com>
2023-08-03 14:33:45 +0200

    doublecross done

--- train_controlnet_pww.py
+++ train_controlnet_pww.py
@@ -1,6 +1,7 @@
 from copy import deepcopy
 from datetime import timedelta
 import json
+import pickle as pkl
 from pathlib import Path
 import random
 from typing import Any, Dict
@@ -20,8 +21,8 @@ from torch.utils.data import DataLoader
 from cldm.cldm import ControlLDM
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
-from dataset import COCODataset, COCODataLoader, ProcessedCOCOExample
-from ldm.modules.attention import default
+from dataset import COCOPanopticDataset, COCODataLoader
+from ldm.modules.attention import BasicTransformerBlock, default
 from ldm.modules.diffusionmodules.util import torch_cat_nested
 from ldm.modules.distributions.distributions import DiagonalGaussianDistribution
 from ldm.util import SeedSwitch, log_txt_as_img
@@ -99,24 +100,26 @@ class CustomCrossAttentionBase(nn.Module):
         m.init_extra()
         return m
     
-    def weight_func(self, sim, context):
+    def init_extra(self):
+        for p in self.get_trainable_parameters():
+            p.train_param = True
+    
+    def weight_func(self, sim, context, sim_mask=None):
         with torch.no_grad():
-            padmask = context.captiontypes >= 0
-            padmask = repeat(padmask, 'b j -> (b h) () j', h=sim.shape[0] // padmask.shape[0])
-            simstd = torch.masked_select(sim, padmask).std()
+            if sim_mask is None:
+                sim_mask = context.captiontypes >= 0
+                sim_mask = repeat(sim_mask, 'b j -> (b h) () j', h=sim.shape[0] // sim_mask.shape[0])
+            simstd = torch.masked_select(sim, sim_mask).std()
             mask = context.cross_attn_masks[sim.shape[1]].to(sim.dtype)
             ret = mask * simstd * context.strength
         return ret
     
     def get_trainable_parameters(self):
-        pass    
+        return []    
 
     
 class CustomCrossAttentionBaseline(CustomCrossAttentionBase):
     # Tries to emulate the basic setting where only the global prompt is available, and attention computation is not changed.
-
-    def init_extra(self):
-        pass
 
     def forward(self, x, context=None, mask=None):
         h = self.heads
@@ -165,14 +168,14 @@ class CustomCrossAttentionBaseline(CustomCrossAttentionBase):
         max_neg_value = -torch.finfo(sim.dtype).max
         mask = context.captiontypes >= 0
         mask = repeat(mask, 'b j -> (b h) () j', h=numheads)
-        sim.masked_fill_(~mask, max_neg_value)
         """ Takes the unscaled unnormalized attention scores computed by cross-attention module, returns adapted attention scores. """
-        wf = self.weight_func(sim, context)
+        wf = self.weight_func(sim, context, sim_mask=mask)
         wf.masked_fill_(~context.global_prompt_mask[:, None, :], max_neg_value)
         
         wf = wf[:, None].repeat(1, numheads, 1, 1)
         wf = wf.view(-1, wf.shape[-2], wf.shape[-1])
         
+        sim.masked_fill_(~mask, max_neg_value)
         sim = sim + wf
         return sim
     
@@ -180,19 +183,6 @@ class CustomCrossAttentionBaseline(CustomCrossAttentionBase):
         params = list(self.to_q.parameters())
         params += list(self.to_k.parameters())
         return params
-    
-    
-class DoubleCrossAttention(CustomCrossAttentionBase):
-    """ First applies trainable cross-attention using local descriptions and then regular frozen global attention"""
-    
-    @classmethod
-    def from_base(cls, m):
-        m.__class__ = cls
-        m.init_extra()
-        return m
-    
-    def init_extra(self):
-        self.local = deepcopy(self)     # this will be the trainable one
     
 
 class CustomCrossAttentionBaselineBoth(CustomCrossAttentionBaseline):
@@ -220,14 +210,50 @@ class CustomCrossAttentionBaselineLocal(CustomCrossAttentionBaseline):
         max_neg_value = -torch.finfo(sim.dtype).max
         mask = context.captiontypes >= 2
         mask = repeat(mask, 'b j -> (b h) () j', h=numheads)
-        sim.masked_fill_(~mask, max_neg_value)
         """ Takes the unscaled unnormalized attention scores computed by cross-attention module, returns adapted attention scores. """
-        wf = self.weight_func(sim, context)
-        
+        wf = self.weight_func(sim, context, sim_mask=mask)
         wf = wf[:, None].repeat(1, numheads, 1, 1)
         wf = wf.view(-1, wf.shape[-2], wf.shape[-1])
         
+        wf.masked_fill_(~mask, max_neg_value)
         sim = sim + wf
+        return sim
+    
+    
+class CustomCrossAttentionBaselineLocalGlobalFallback(CustomCrossAttentionBaseline):
+    """ Uses only local descriptions, unless there is none (all local ones are masked), then falls back to global description."""
+    
+    def cross_attention_control(self, sim, context, numheads=None):
+        # compute mask that ignores everything except the local descriptions
+        max_neg_value = -torch.finfo(sim.dtype).max
+        mask = context.captiontypes >= 2
+        mask = repeat(mask, 'b j -> (b h) () j', h=numheads)
+        
+        # get mask that selects global as well as applicable local prompt
+        wf = self.weight_func(sim, context, sim_mask=mask)
+
+        # expand dimensions to match heads
+        wf = wf[:, None].repeat(1, numheads, 1, 1)
+        wf = wf.view(-1, wf.shape[-2], wf.shape[-1])
+        
+        # remove the global part from the mask
+        wf.masked_fill_(~mask, 0)  # max_neg_value)
+        # determine where to use global mask (where no local descriptions are available)
+        useglobal = wf.sum(-1) == 0
+        
+        # get the mask back to 0/1, make sure we only attend to at most one of the local descriptions at once
+        wfmaxes = wf.max(-1, keepdim=True)[0]
+        wf = (wf >= (wfmaxes.clamp_min(1e-3) - 1e-4)).float()
+        
+        # get mask that selects only global tokens
+        gmask = (context.captiontypes >= 0) & (context.captiontypes < 2)
+        gmask = repeat(gmask, 'b j -> (b h) () j', h=numheads)
+        
+        # update stimulation to attend to global tokens when no local tokens are available
+        wf = wf + (useglobal[:, :, None] & gmask)
+        
+        sim.masked_fill_(wf == 0, max_neg_value)
+        # sim = sim + wf
         return sim
     
     
@@ -241,6 +267,73 @@ class CustomCrossAttentionBaselineGlobal(CustomCrossAttentionBaseline):
         sim.masked_fill_(~mask, max_neg_value)
         return sim
     
+    
+class DoublecrossBasicTransformerBlock(BasicTransformerBlock):
+    @classmethod
+    def convert(cls, m):
+        m.__class__ = cls
+        m.init_extra()
+        return m
+    
+    def init_extra(self):
+        self.attn2l = deepcopy(self.attn2)
+        self.attn2.__class__ = CustomCrossAttentionBaselineGlobal
+        self.attn2l.__class__ = CustomCrossAttentionBaselineLocalGlobalFallback
+        
+        self.register_buffer("manual_gate", torch.tensor([1.]))
+        self.learned_gate = torch.nn.Parameter(torch.tensor([0.]))      # TODO: per-head learned gate
+        
+        self.norm2l = deepcopy(self.norm2)
+        
+        for p in self.get_trainable_parameters():
+            p.train_param = True
+        
+    def _forward(self, x, context=None):
+        x = self.attn1(self.norm1(x), context=context if self.disable_self_attn else None) + x
+        x = self.manual_gate * torch.tanh(self.learned_gate) * self.attn2l(self.norm2l(x), context=context) + x
+        x = self.attn2(self.norm2(x), context=context) + x
+        x = self.ff(self.norm3(x)) + x
+        return x
+    
+    def get_trainable_parameters(self):
+        params = list(self.attn2l.parameters())
+        params += list(self.norm2l.parameters())
+        params += [self.learned_gate]
+        return params   
+    
+    
+# class DoubleCrossAttention(CustomCrossAttentionBaselineGlobal):         # TODO remove this
+#     """ First applies trainable cross-attention using local descriptions and then regular frozen global attention"""
+    
+#     @classmethod
+#     def from_base(cls, m):
+#         m.__class__ = cls
+#         m.init_extra()
+#         return m
+    
+#     def init_extra(self):
+#         self.local = deepcopy(self)     # this will be the trainable one
+#         self.local.__class__ = CustomCrossAttentionBaselineLocalGlobalFallback
+        
+#         self.register_buffer("manual_gate", torch.tensor([1.]))
+#         self.learned_gate = torch.nn.Parameter(torch.tensor([0.]))
+        
+#         for p in self.get_trainable_parameters():
+#             p.train_param = True
+        
+#     def forward(self, x, context=None, mask=None):
+#         # 1. apply (trainable) local attention first
+#         localout = self.local(self.norm1(x), context=context, mask=mask)
+#         x = x + self.manual_gate * self.learned_gate * localout
+#         # 2. apply self (non-trainable) global-only attention
+#         globalout = super().forward(self.norm2(x), context=context, mask=mask)
+#         x = x + globalout
+#         return globalout 
+    
+#     def get_trainable_parameters(self):
+#         params = list(self.local.parameters())
+#         return params   
+
         
 class TokenTypeEmbedding(torch.nn.Module):
     def __init__(self, embdim):
@@ -292,6 +385,9 @@ class CustomCrossAttentionExt(CustomCrossAttentionBase):
         self.token_type_emb = TokenTypeEmbedding(self.to_k.in_features)
         # conditioning on progress (0..1)
         self.progress_emb = ProgressEmbedding(self.to_q.in_features)
+        
+        for p in self.get_trainable_parameters():
+            p.train_param = True
 
     def forward(self, x, context=None, mask=None):
         h = self.heads
@@ -397,6 +493,15 @@ class CustomCrossAttentionMinimal(CustomCrossAttentionExt):
         # gate parameters: one trainable scalar for every head in this attention layer
         self.gate = torch.nn.Parameter(torch.randn(self.heads) * 1e-3)      # 
         
+        for p in self.get_trainable_parameters():
+            p.train_param = True
+        
+    def get_trainable_parameters(self):
+        params = list(self.token_type_emb.parameters())
+        params += list(self.progress_emb.parameters())
+        params += [self.gate]
+        return params
+        
     def forward(self, x, context=None, mask=None):
         h = self.heads
         
@@ -473,12 +578,6 @@ class CustomCrossAttentionMinimal(CustomCrossAttentionExt):
         
         sim = sim + wf
         return sim
-    
-    def get_trainable_parameters(self):
-        params = list(self.token_type_emb.parameters())
-        params += list(self.progress_emb.parameters())
-        params += [self.gate]
-        return params
 
 
 class ControlPWWLDM(ControlLDM):
@@ -606,21 +705,26 @@ class ControlPWWLDM(ControlLDM):
         return eps
     
     def get_trainable_parameters(self):
-        params = []
-        # select query and key projections as well as new modules from CustomCrossAttention
-        for module in self.modules():
-            if isinstance(module, CustomCrossAttentionBase):
-                params += list(module.get_trainable_parameters())
+        # params = []
+        # # select query and key projections as well as new modules from CustomCrossAttention
+        # for param in self.parameters():
+        #     if hasattr(param, "train_param") and param.train_param:
+        #         params += param
+        # for module in self.modules():
+        #     if isinstance(module, (DoublecrossBasicTransformerBlock, CustomCrossAttentionBase)):
+        #         params += list(module.get_trainable_parameters())
                 
-        for param in params:
-            param.store_param = True
-            
-        saved_param_names = set()
-        for paramname, param in self.named_parameters():
-            if hasattr(param, "store_param") and param.store_param:
-                saved_param_names.add(paramname)
+        # for param in params:
+        #     param.store_param = True
         
-        return params, saved_param_names
+        params = []
+        saved_param_names = []
+        for paramname, param in self.named_parameters():
+            if hasattr(param, "train_param") and param.train_param:
+                saved_param_names.append(paramname)
+                params.append(param)
+        
+        return params, set(saved_param_names)
     
     def configure_optimizers(self):
         lr = self.learning_rate
@@ -824,7 +928,7 @@ def convert_model(model, cas_class=None, cas_name=None, freezedown=False, simple
                      "global": CustomCrossAttentionBaselineGlobal,
                      "bothext": CustomCrossAttentionExt,
                      "bothminimal": CustomCrossAttentionMinimal,
-                     "doublecross": CustomCrossAttentionBaselineGlobal}[cas_name]
+                     "doublecross": None}[cas_name]
         
     if cas_class is None:
         cas_class = CustomCrossAttentionBaseline
@@ -834,14 +938,20 @@ def convert_model(model, cas_class=None, cas_name=None, freezedown=False, simple
     
     # DONE: replace CrossAttentions that are at attn2 in BasicTransformerBlocks with adapted CustomCrossAttention that takes into account cross-attention masks
     for module in model.model.diffusion_model.modules():
-        if module.__class__.__name__ == "BasicTransformerBlock":
+        if isinstance(module, BasicTransformerBlock): # module.__class__.__name__ == "BasicTransformerBlock":
             assert not module.disable_self_attn
-            module.attn2 = cas_class.from_base(module.attn2)
+            if cas_name == "doublecross":
+                DoublecrossBasicTransformerBlock.convert(module)
+            else:
+                module.attn2 = cas_class.from_base(module.attn2)
             
     for module in model.control_model.modules():
-        if module.__class__.__name__ == "BasicTransformerBlock":
+        if isinstance(module, BasicTransformerBlock): # module.__class__.__name__ == "BasicTransformerBlock":
             assert not module.disable_self_attn
-            module.attn2 = cas_class.from_base(module.attn2)
+            if cas_name == "doublecross":
+                DoublecrossBasicTransformerBlock.convert(module)
+            else:
+                module.attn2 = cas_class.from_base(module.attn2)
     
     return model
 
@@ -882,7 +992,9 @@ def create_controlnet_pww_model(basemodelname="v1-5-pruned.ckpt", model_name='co
 
 
 def main(batsize=5,
-         version="v1",
+         version="v2",
+         datadir="/USERSPACE/lukovdg1/coco2017/",
+         devexamples="coco2017.4dev.examples.pkl",
          cas="doublecross",  # "both", "local", "global", "bothext", "bothminimal", "doublecross"
          devices=(0,),
          numtrain=-1,
@@ -909,12 +1021,9 @@ def main(batsize=5,
     numtrain = None if numtrain == -1 else numtrain
     
     # check dataloader
-    # ds = COCODataset(split="train" if forreal else "valid", cas=cas, simpleencode=simpleencode, 
-    #                  max_samples=numtrain if numtrain is not None else (None if forreal else 250))
-    if forreal:
-        ds = COCODataset.from_cache("coco2017.train.cached", casmode=cas, simpleencode=simpleencode)
-    else:
-        ds = COCODataset.from_cache("coco2017.valid.cached", casmode=cas, simpleencode=simpleencode)
+    ds = COCOPanopticDataset(maindir=datadir, split="train" if forreal else "valid", casmode=cas, simpleencode=simpleencode, 
+                     max_samples=numtrain if numtrain is not None else (None if forreal else 1000))
+    
     print(len(ds))
     batsizes = {384: round(batch_size * 2.4), 448:round(batch_size * 1.4), 512: batch_size}
     print(f"Batch sizes: {batsizes}")
@@ -922,9 +1031,12 @@ def main(batsize=5,
                         num_workers=max(batsizes.values()),
                         shuffle=True)
     
-    # valid_ds = COCODataset(split="valid", max_samples=600, min_size=512, cas=cas, simpleencode=simpleencode)
-    valid_ds = COCODataset.from_cache("coco2017.valid.cached", casmode=cas, simpleencode=simpleencode)
-    valid_dl = COCODataLoader(valid_ds, batch_size=4, num_workers=batsize, shuffle=False)
+    # load dev set from pickle
+    with open(devexamples, "rb") as f:
+        loadedexamples = pkl.load(f)
+    # override pickled defaults
+    valid_ds = COCOPanopticDataset(examples=loadedexamples, casmode=cas, simpleencode=simpleencode)
+    valid_dl = COCODataLoader(valid_ds, batch_size=4, num_workers=4, shuffle=False)
 
     model = create_controlnet_pww_model(cas_name=cas, freezedown=freezedown, simpleencode=simpleencode)
 
@@ -935,7 +1047,7 @@ def main(batsize=5,
     expnr = 1
     
     def get_exp_name(_expnr):
-        ret = f"checkpoints_coco_{cas}_{version}_exp_{_expnr}{'_forreal' if forreal else ''}"
+        ret = f"checkpoints/{version}/checkpoints_coco_{cas}_{version}_exp_{_expnr}{'_forreal' if forreal else ''}"
         if freezedown:
             ret += "_freezedown"
         if simpleencode:
@@ -987,9 +1099,12 @@ if __name__ == "__main__":
     
     # TODO: train CAS better by dropping out the ControlNet control (because ControlNet already implies some object identity with its shapes)
     
+    
     # TODO: IDEA: double-cross-attention: one cross attention on global prompt (unchanged, untrained?), and another cross-attention on local prompts
     
-    # TODO: validation setup: take images of apples and oranges and of cats and dogs and change where the cats and dogs and apples and oranges are
+    # DONE: validation setup: take images of apples and oranges and of cats and dogs and change where the cats and dogs and apples and oranges are
+    # DONE: use panoptic segmentation data from COCO instead
+    # TODO(?): drop some regions
     
     # TODO: evaluation setup --> RegionCLIP?
     
