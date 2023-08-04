many things + v3: fixed mistake where no gradient backward beyond controlled mid block

cldm/logger.py
@@ -39,57 +39,40 @@ class ImageLogger(Callback):
         self.log_first_step = log_first_step
         self.dl = dl    # dataloader instead of training batch
         self.seed = seed
+        self.batch_size = self.dl.batch_size
 
     @rank_zero_only
     def log_local(self, save_dir, split, images, global_step, current_epoch, batch_idx):
         root = os.path.join(save_dir, "image_log", split)
         for k in images:
-            grid = torchvision.utils.make_grid(images[k], nrow=4)
+            grid = torchvision.utils.make_grid(images[k], nrow=self.batch_size)
             if self.rescale:
                 grid = (grid + 1.0) / 2.0  # -1,1 -> 0,1; c,h,w
             grid = grid.transpose(0, 1).transpose(1, 2).squeeze(-1)
             grid = grid.numpy()
             grid = (grid * 255).astype(np.uint8)
-            filename = "{}_gs-{:06}_e-{:06}_b-{:06}.png".format(k, global_step, current_epoch, batch_idx)
+            filename = "{}_gs-{:06}_e-{:03}_b-{:06}.png".format(k, global_step, current_epoch, batch_idx)
             path = os.path.join(root, filename)
             os.makedirs(os.path.split(path)[0], exist_ok=True)
             Image.fromarray(grid).save(path)
-
-    def log_img(self, pl_module, batch, batch_idx, split="train"):
-        if isinstance(self.seed, SeedSwitch):
-            self.seed.__enter__()
-        else:
-            seed_everything(self.seed)
-        check_idx = batch_idx  # if self.log_on_batch_idx else pl_module.global_step
-        if (self.check_frequency(check_idx) and  # batch_idx % self.batch_freq == 0
-                hasattr(pl_module, "log_images") and
-                callable(pl_module.log_images) and
-                self.max_images > 0):
-            logger = type(pl_module.logger)
-
-            is_train = pl_module.training
-            if is_train:
-                pl_module.eval()
-                
-            device = pl_module.device
-            N = None
-            prevbatch = None
-            for batch in iter(self.dl):
-                bs = batch["image"].shape[0] 
-                if N is None:
-                    N = bs
-                if bs < N:
-                    break
-                prevbatch = batch
-            # batch = next(iter(self.dl))
-            batch = prevbatch
+            
+    def do_log_img(self, pl_module, split="train"):
+        is_train = pl_module.training
+        if is_train:
+            pl_module.eval()
+            
+        device = pl_module.device
+        numgenerated = 0
+        
+        for batch_idx, batch in enumerate(iter(self.dl)):
             batch = nested_to(batch, device)
 
             with torch.no_grad():
                 images = pl_module.log_images(batch, split=split, **self.log_images_kwargs)
 
             for k in images:
-                N = min(images[k].shape[0], self.max_images)
+                N = images[k].shape[0]
+                numgenerated += N
                 images[k] = images[k][:N]
                 if isinstance(images[k], torch.Tensor):
                     images[k] = images[k].detach().cpu()
@@ -97,10 +80,27 @@ class ImageLogger(Callback):
                         images[k] = torch.clamp(images[k], -1., 1.)
 
             self.log_local(pl_module.logger.save_dir, split, images,
-                           pl_module.global_step, pl_module.current_epoch, batch_idx)
+                        pl_module.global_step, pl_module.current_epoch, batch_idx)
+            
+            if numgenerated >= self.max_images:
+                break
 
-            if is_train:
-                pl_module.train()
+        if is_train:
+            pl_module.train()
+
+    def log_img(self, pl_module, global_step, split="train"):
+        if isinstance(self.seed, SeedSwitch):
+            self.seed.__enter__()
+        else:
+            seed_everything(self.seed)
+        # if self.log_on_batch_idx else pl_module.global_step
+        if (self.check_frequency(global_step) and  # batch_idx % self.batch_freq == 0
+                hasattr(pl_module, "log_images") and
+                callable(pl_module.log_images) and
+                self.max_images > 0):
+            # logger = type(pl_module.logger)
+            self.do_log_img(pl_module=pl_module, split=split)
+            
         if isinstance(self.seed, SeedSwitch):
             self.seed.__exit__(None, None, None)
 
@@ -115,4 +115,4 @@ class ImageLogger(Callback):
     def on_train_batch_end(self, trainer, pl_module, outputs, batch, batch_idx, dataloader_idx):
         if not self.disabled:
             # self.log_img(pl_module, batch, batch_idx, split="train")
-            self.log_img(pl_module, batch, trainer.global_step, split="train")      # Fixed logging to actually log every N steps
+            self.log_img(pl_module, trainer.global_step, split="train")      # Fixed logging to actually log every N steps

train_controlnet_pww.py
@@ -1082,7 +1082,7 @@ def get_checkpointing_callbacks(interval=6*60*60, dirpath=None):
 
 
 def create_controlnet_pww_model(basemodelname="v1-5-pruned.ckpt", model_name='control_v11p_sd15_seg', cas_name="bothext",
-                                freezedown=False, simpleencode=False, threshold=-1):
+                                freezedown=False, simpleencode=False, threshold=-1, loadckpt=""):
     # First use cpu to load models. Pytorch Lightning will automatically move it to GPUs.
     model = create_model(f'./models/{model_name}.yaml').cpu()
     # load main weights
@@ -1093,14 +1093,24 @@ def create_controlnet_pww_model(basemodelname="v1-5-pruned.ckpt", model_name='co
     model.controlnet_model_name = model_name
     
     model = convert_model(model, cas_name=cas_name, freezedown=freezedown, simpleencode=simpleencode, threshold=threshold)
+    
+    if loadckpt != "":
+        refparam1a = model.model.diffusion_model.middle_block[1].proj_in.weight.data.clone()
+        refparam2a = deepcopy(model.model.diffusion_model.middle_block[1].transformer_blocks[0].attn2l.to_q.weight.data.clone())
+        ckpt_state_dict = load_state_dict(loadckpt, location="cpu")
+        # testing the partial loading
+        model.load_state_dict(ckpt_state_dict, strict=False)
+        refparam1b = model.model.diffusion_model.middle_block[1].proj_in.weight.data.clone()
+        refparam2b = deepcopy(model.model.diffusion_model.middle_block[1].transformer_blocks[0].attn2l.to_q.weight.data.clone())
+        assert torch.all(refparam1a == refparam1b)
     return model
 
 
 def main(batsize=5,
-         version="v2",
+         version="v3",
          datadir="/USERSPACE/lukovdg1/coco2017/",
          devexamples="coco2017.4dev.examples.pkl",
-         cas="sepswitch",  # "both", "local", "global", "bothext", "bothminimal", "doublecross", "sepswitch"
+         cas="doublecross",  # "both", "local", "global", "bothext", "bothminimal", "doublecross", "sepswitch"
          devices=(0,),
          numtrain=-1,
          forreal=False,
@@ -1109,9 +1119,9 @@ def main(batsize=5,
          freezedown=False,      # don't adapt the down-sampling blocks of the Unet, only change and train the upsamling blocks
          simpleencode=False,    # encode both global and all local prompts as one sequence
         #  minimal=False,         # only interaction between layer+head info, progress and token type (global/local/bos) (<-- essentially a learned schedule for CAS, independent of content)
-         generate="",
+         generate="dev",   # ""
          threshold=-1,
-         loadckpt="",
+         loadckpt="", #"/USERSPACE/lukovdg1/controlnet11/checkpoints/v2/checkpoints_coco_doublecross_v2_exp_1_forreal/interval_delta_epoch=epoch=5_step=step=64069.ckpt", #"",
          ):  
     args = locals().copy()
     # print(args)
@@ -1155,48 +1165,54 @@ def main(batsize=5,
     valid_ds = COCOPanopticDataset(examples=loadedexamples, casmode=cas, simpleencode=simpleencode)
     valid_dl = COCODataLoader(valid_ds, batch_size=4, num_workers=4, shuffle=False)
     
-    model = create_controlnet_pww_model(cas_name=cas, freezedown=freezedown, simpleencode=simpleencode, threshold=threshold)
+    model = create_controlnet_pww_model(cas_name=cas, freezedown=freezedown, simpleencode=simpleencode, 
+                                        threshold=threshold, loadckpt=loadckpt)
     
     seedswitch = SeedSwitch(seed, log_image_seed)
     image_logger = ImageLogger(batch_frequency=logger_freq, dl=valid_dl, seed=seedswitch)
-        
-    ds = COCOPanopticDataset(maindir=datadir, split="train" if forreal else "valid", casmode=cas, simpleencode=simpleencode, 
-                    max_samples=numtrain if numtrain is not None else (None if forreal else 1000))
     
-    print(len(ds))
-    batsizes = {384: round(batch_size * 2.4), 448:round(batch_size * 1.4), 512: batch_size}
-    print(f"Batch sizes: {batsizes}")
-    dl = COCODataLoader(ds, batch_size=batsizes, 
-                        num_workers=max(batsizes.values()),
-                        shuffle=True)
-
-    model.learning_rate = learning_rate if not generate else 0
-    model.sd_locked = sd_locked
-        
-    checkpoints = get_checkpointing_callbacks(interval=8*60*60 if forreal else 60*10, dirpath=exppath)
-    logger = pl.loggers.TensorBoardLogger(save_dir=exppath)
-    
-    max_steps = -1
     if generate:
-        max_steps = 1
+        exppath.mkdir(parents=True, exist_ok=False)
+        image_logger.do_log_img(model, split="gen")
     
-    trainer = pl.Trainer(accelerator="gpu", devices=devices, 
-                        precision=32, max_steps=max_steps,
-                        logger=logger,
-                        callbacks=checkpoints + [image_logger])
+    else:
+        ds = COCOPanopticDataset(maindir=datadir, split="train" if forreal else "valid", casmode=cas, simpleencode=simpleencode, 
+                        max_samples=numtrain if numtrain is not None else (None if forreal else 1000))
+        
+        print(len(ds))
+        batsizes = {384: round(batch_size * 2.4), 448:round(batch_size * 1.4), 512: batch_size}
+        print(f"Batch sizes: {batsizes}")
+        dl = COCODataLoader(ds, batch_size=batsizes, 
+                            num_workers=max(batsizes.values()),
+                            shuffle=True)
 
-    # Train!
-    print(f"Writing to {exppath}")
-    model.save_hyperparameters()
-    # with open(exppath / "args.json", "w") as f:
-    #     json.dump(args, f)
-    trainer.fit(model, dl)
-    
-    # # generate
-    # device = torch.device("cuda", devices[0])
-    # print("device", device)
-    # model = model.to(device)
-    # image_logger.log_img(model, None, 0, split="dev")
+        model.learning_rate = learning_rate if not generate else 0
+        model.sd_locked = sd_locked
+            
+        checkpoints = get_checkpointing_callbacks(interval=8*60*60 if forreal else 60*10, dirpath=exppath)
+        logger = pl.loggers.TensorBoardLogger(save_dir=exppath)
+        
+        max_steps = -1
+        if generate:
+            max_steps = 1
+        
+        trainer = pl.Trainer(accelerator="gpu", devices=devices, 
+                            precision=32, max_steps=max_steps,
+                            logger=logger,
+                            callbacks=checkpoints + [image_logger])
+
+        # Train!
+        print(f"Writing to {exppath}")
+        
+        # with open(exppath / "args.json", "w") as f:
+        #     json.dump(args, f)
+        trainer.fit(model, dl)
+        
+        # # generate
+        # device = torch.device("cuda", devices[0])
+        # print("device", device)
+        # model = model.to(device)
+        # image_logger.log_img(model, None, 0, split="dev")
     
     
 if __name__ == "__main__":